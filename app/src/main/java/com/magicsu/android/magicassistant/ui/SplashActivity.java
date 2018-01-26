ca407fa35feb1746987ad32f79e68eb3ca608155
suzhenhui <362680581@qq.com>
2018-01-26 17:31:53 +0800

    # 闪屏activity

---
+++ app/src/main/java/com/magicsu/android/magicassistant/ui/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.magicsu.android.magicassistant.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * project: MagicAssistant
+ * package: com.magicsu.android.magicassistant.ui
+ * file: SplashActivity
+ * author: admin
+ * date: 2018/1/26
+ * description: 引导页
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
